38cf984fa2ad46569685514af8d6503237c4ab26
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-01-19 10:29:22 -0500

    Update vite.config.js
    import { defineConfig } from 'vite'
    import react from '@vitejs/plugin-react'
    
    export default defineConfig({
      plugins: [react()],
      base: '/FleetFlow/',
      server: {
        host: true,
        port: 3000
      }
    })

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FleetFlow/',
   server: {
-    host: true, 
+    host: true,
     port: 3000
   }
 })
-
-
