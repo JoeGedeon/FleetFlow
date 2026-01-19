00b3a44eead31832ac65b123d0ed83e709226345
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-01-19 01:37:13 -0500

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Home from './pages/Home';
-
-export default function App() {
-  return (
-    <div style={{ minHeight: '100vh', background: '#0a0a0a', color: '#fff' }}>
-      <Home />
-    </div>
-  );
-}
