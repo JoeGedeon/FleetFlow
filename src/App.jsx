25c53aadee33d4a0a9f7fe417bf5a6c4627b342a
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-01-19 00:15:09 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#0a0a0a', color: '#fff' }}>
+      <Home />
+    </div>
+  );
+}
