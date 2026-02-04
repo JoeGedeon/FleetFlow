b67283f99be6b77776020dc4473ef0ef9c011f99
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2026-02-04 14:50:58 -0500

    Update main.jsx
    import App from './App_001.js'  // ✅ Correct - just "./" since we're already in src/

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src./app_001.js'  // ✅ Changed this line
+import App from './app_001.js'  // ✅ Correct - just "./" since we're already in srca
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
